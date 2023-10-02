b9113da5c3231c8f48abba2282d7af13702e7d2c
Taj <tajmarie92@gmail.com>
2023-10-02 13:25:27 -0500

    Completed Exercise 1 of 2

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Let's play a number quessing game!");
+           var  r = new Random();
+           var favNumber = r.Next(1,1000);
+           int userInput = int.Parse(Console.ReadLine());
+
+            if (userInput < 907) 
+            { 
+                Console.WriteLine("Too Low");
+
+            }
+
+            else if (userInput > 907)
+            {
+                Console.WriteLine("Too High");
+            }
+
+            else if(userInput == 907)
+            {
+                Console.WriteLine("You Guessed It, Great Job!");
+            }
         }
     }
 }
